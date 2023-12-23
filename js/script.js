c79e2abf7fbd8c11763ee019015bace1044c0315
Sabina Nuhanovic <96692767+Sabina1205@users.noreply.github.com>
2023-12-23 18:19:19 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+document.onreadystatechange = function () {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loader").style.visibility = "visible";
+    } else {
+        document.querySelector("#loader").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+};
